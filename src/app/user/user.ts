1d67a2c36cc6a3f1119251dcb9c706e4956bb84f
D.C. Pagan <dcpagan@posteo.net>
2026-03-01 22:15:59 -0500

    Use inputs

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,7 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { DUMMY_USERS, UserData } from '../dummy-users';
-
-const randomIndex: number = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input, Signal, computed } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,13 +7,11 @@ const randomIndex: number = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.scss',
 })
 export class User {
-  @Input() id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input() id?: string
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-  get imagePath(): string {
-    return `users/${this.avatar}`;
-  }
+  imagePath: Signal<string> = computed(() => `users/${this.avatar}`);
 
   onSelectUser(): void {}
 }
